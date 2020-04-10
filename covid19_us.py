145d4ebe288cc2a11dd0916d466624bae40d121e
Matthew Burger <mburger@stsci.edu>
2020-04-10 05:10:02 -0400

    created covid19_us.py - US stats

--- covid19_us.py
+++ covid19_us.py
@@ -13,9 +13,8 @@ from bokeh.layouts import Column
 curdoc().theme = Theme('/Users/mburger/.bokeh/bokeh.yml')
 
 def makedate(datestr):
-    result = [int(x) for x in datestr.split('-')]
-
-    return date(int(yr), months[mon], int(day.replace(',', '')))
+    result = [int(x) for x in datestr.split('/')]
+    return date(result[2]+2000, result[0], result[1])
 
 
 cases = pd.read_csv('https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv')
@@ -24,18 +23,32 @@ population = pd.read_csv('https://usafactsstatic.blob.core.windows.net/public/da
                          '/covid_county_population_usafacts.csv')
 
 states = cases.State.unique()
-cases_state = pd.DataFrame(index=states)
+dates_ = [x for x in cases.columns if '/' in x]
+dates = [makedate(x) for x in cases.columns if '/' in x]
+columns = ['Date', 'State', 'new_cases', 'total_cases', 'new_deaths',
+           'total_deaths']
+cases_state = pd.DataFrame(columns=columns)
 
+for state in states:
+    c_cum = 0
+    d_cum = 0
+    for day_, day in zip(dates_, dates):
+        c_new = cases[(cases.State == state)][day_].sum()
+        c_cum += c_new
+        d_new = deaths[(deaths.State == state)][day_].sum()
+        d_cum += d_new
+        cases_state = cases_state.append({'Date': day, 'State': state,
+                                          'new_cases': c_new,
+                                          'total_cases': c_cum,
+                                          'new_deaths': d_new,
+                                          'total_deaths': d_cum},
+        ignore_index=True)
 
+cases_state['DateIso'] = cases_state.Date.apply(lambda x: x.isoformat())
 
-
-data['Date'] = data.date.apply(lambda x:
-                            date(*[int(y) for y in x.split('-')]))
-
-codes_to_plot = ['United States', 'South Korea', 'Italy',
-                 'United Kingdom', 'Japan', 'Spain']
-tooltips = [('Country', '@location'),
-            ('Date', '@date'),
+states_to_plot = ['MD', 'NY', 'FL', 'WA', 'TX', 'LA', 'CA']
+tooltips = [('State', '@State'),
+            ('Date', '@DateIso'),
             ('New Cases', '@new_cases'),
             ('Total Cases', '@total_cases')]
 
@@ -47,48 +60,50 @@ fig0 = bkp.figure(plot_width=1200, plot_height=600,
                   tooltips=tooltips)
 
 colors = (c for c in Category10[10])
-for code in codes_to_plot:
-    plotdata = ColumnDataSource(data[data.location == code])
+for state in states_to_plot:
+    plotdata = ColumnDataSource(cases_state[cases_state.State == state])
     c = next(colors)
     fig0.circle('Date', 'total_cases', source=plotdata, color=c,
-               legend_label=code)
+               legend_label=state)
     fig0.line('Date', 'total_cases', source=plotdata, color=c,
-              legend_label=code)
+              legend_label=state)
 
 fig1 = bkp.figure(plot_width=1200, plot_height=600,
                   title='New Cases',
                   x_axis_label='Date',
                   y_axis_label='# Cases',
                   x_axis_type='datetime', y_axis_type='log',
+                  # x_range=fig0.x_range, y_range=fig0.y_range,
                   tooltips=tooltips)
 
 colors = (c for c in Category10[10])
-for code in codes_to_plot:
-    plotdata = ColumnDataSource(data[data.location == code])
+for state in states_to_plot:
+    plotdata = ColumnDataSource(cases_state[cases_state.State == state])
     c = next(colors)
     fig1.circle('Date', 'new_cases', source=plotdata, color=c,
-                legend_label=code)
+                legend_label=state)
     fig1.line('Date', 'new_cases', source=plotdata, color=c,
-                legend_label=code)
+                legend_label=state)
 
 fig2 = bkp.figure(plot_width=1200, plot_height=600,
                   title='New Cases vs. Total Cases',
                   x_axis_label='Total Cases',
                   y_axis_label='New Cases',
                   x_axis_type='log', y_axis_type='log',
+                  # x_range=fig0.x_range, y_range=fig0.y_range,
                   tooltips=tooltips)
 
 colors = (c for c in Category10[10])
-for code in codes_to_plot:
-    plotdata = ColumnDataSource(data[data.location == code])
+for state in states_to_plot:
+    plotdata = ColumnDataSource(cases_state[cases_state.State== state])
     c = next(colors)
     fig2.circle('total_cases', 'new_cases', source=plotdata, color=c,
-                legend_label=code)
+                legend_label=state)
     fig2.line('total_cases', 'new_cases', source=plotdata, color=c,
-                legend_label=code)
+                legend_label=state)
 
-tooltips = [('Country', '@location'),
-            ('Date', '@date'),
+tooltips = [('Country', '@State'),
+            ('Date', '@DateIso'),
             ('New Deaths', '@new_deaths'),
             ('Total Deaths', '@total_deaths')]
 
@@ -97,48 +112,52 @@ fig3 = bkp.figure(plot_width=1200, plot_height=600,
                   x_axis_label='Date',
                   y_axis_label='# Deaths',
                   x_axis_type='datetime', y_axis_type='log',
+                  # x_range=fig0.x_range, y_range=fig0.y_range,
                   tooltips=tooltips)
 
 colors = (c for c in Category10[10])
-for code in codes_to_plot:
-    plotdata = ColumnDataSource(data[data.location == code])
+for state in states_to_plot:
+    plotdata = ColumnDataSource(cases_state[cases_state.State == state])
     c = next(colors)
     fig3.circle('Date', 'total_deaths', source=plotdata, color=c,
-                legend_label=code)
+                legend_label=state)
     fig3.line('Date', 'total_deaths', source=plotdata, color=c,
-              legend_label=code)
+              legend_label=state)
 
 fig4 = bkp.figure(plot_width=1200, plot_height=600,
                   title='New Deaths',
                   x_axis_label='Date',
                   y_axis_label='# Deaths',
                   x_axis_type='datetime', y_axis_type='log',
+                  # x_range=fig0.x_range, y_range=fig0.y_range,
                   tooltips=tooltips)
 
 colors = (c for c in Category10[10])
-for code in codes_to_plot:
-    plotdata = ColumnDataSource(data[data.location == code])
+for state in states_to_plot:
+    plotdata = ColumnDataSource(cases_state[cases_state.State == state])
     c = next(colors)
     fig4.circle('Date', 'new_deaths', source=plotdata, color=c,
-                legend_label=code)
+                legend_label=state)
     fig4.line('Date', 'new_deaths', source=plotdata, color=c,
-              legend_label=code)
+              legend_label=state)
 
 fig5 = bkp.figure(plot_width=1200, plot_height=600,
                   title='New Deaths vs. Total Deaths',
                   x_axis_label='Total Deaths',
                   y_axis_label='New Deaths',
                   x_axis_type='log', y_axis_type='log',
+                  # x_range=fig0.x_range, y_range=fig0.y_range,
                   tooltips=tooltips)
 
 colors = (c for c in Category10[10])
-for code in codes_to_plot:
-    plotdata = ColumnDataSource(data[data.location == code])
+for state in states_to_plot:
+    plotdata = ColumnDataSource(cases_state[cases_state.State == state])
     c = next(colors)
     fig5.circle('total_deaths', 'new_deaths', source=plotdata, color=c,
-                legend_label=code)
+                legend_label=state)
     fig5.line('total_deaths', 'new_deaths', source=plotdata, color=c,
-              legend_label=code)
+              legend_label=state)
+
 
 fig0.legend.location = "top_left"
 fig0.legend.click_policy="hide"
@@ -153,5 +172,6 @@ fig4.legend.click_policy="hide"
 fig5.legend.location = "top_left"
 fig5.legend.click_policy="hide"
 
-bkp.output_file('covid19.html')
+bkp.output_file('covid19_us.html')
 bkp.show(Column(fig0, fig1, fig2, fig3, fig4, fig5))
+
